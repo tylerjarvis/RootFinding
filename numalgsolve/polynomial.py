878bfbaf9b8c09efe32326bae618bce050a817ea
Tyler Moncur <mtmoncur@gmail.com>
2018-06-17 13:13:23 -0600

    add getpoly function to polynomial

--- numalgsolve/polynomial.py
+++ numalgsolve/polynomial.py
@@ -60,6 +60,20 @@ def chebval2(x, cc): #pragma: no cover
             c1 = tmp + c1*x2
     return c0 + c1*x
 
+def getPoly(deg,dim,power):
+    '''
+    A helper function for testing. Returns a random upper triangular polynomial of the given dimension and degree.
+    power is a boolean indicating whether or not the polynomial should be MultiPower.
+    '''
+    deg += 1
+    ACoeff = np.random.random_sample(deg*np.ones(dim, dtype = int))
+    for i,j in np.ndenumerate(ACoeff):
+        if np.sum(i) >= deg:
+            ACoeff[i] = 0
+    if power:
+        return MultiPower(ACoeff)
+    else:
+        return MultiCheb(ACoeff)
 
 class Polynomial(object):
     '''
