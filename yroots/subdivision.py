0259221781d44ea198b9aac11af4753f8113880f
Sue <suzannastep@gmail.com>
2020-01-30 01:57:08 -0700

    tracks condeig

--- yroots/subdivision.py
+++ yroots/subdivision.py
@@ -23,7 +23,7 @@ import itertools
 import time
 import warnings
 
-def solve(funcs, a, b, rel_approx_tol=1.e-8, abs_approx_tol=1.e-12,
+def solve(method, funcs, a, b, rel_approx_tol=1.e-8, abs_approx_tol=1.e-12,
           trim_zero_tol=1.e-10, max_cond_num=1e5, macaulay_zero_tol=1.e-13,
           good_zeros_factor=100, min_good_zeros_tol=1e-5,
           check_eval_error=True, check_eval_freq = 1, plot = False,
@@ -134,7 +134,7 @@ def solve(funcs, a, b, rel_approx_tol=1.e-8, abs_approx_tol=1.e-12,
         solve_func = subdivision_solve_nd
 
     #Initial Solve
-    solve_func(funcs,a,b,deg,interval_data,root_tracker,tols,max_level)
+    solve_func(method,funcs,a,b,deg,interval_data,root_tracker,tols,max_level)
     root_tracker.keep_possible_duplicates()
 
     #Polishing
@@ -143,7 +143,7 @@ def solve(funcs, a, b, rel_approx_tol=1.e-8, abs_approx_tol=1.e-12,
         interval_data.add_polish_intervals(polish_intervals)
         for new_a, new_b in polish_intervals:
             interval_data.start_polish_interval()
-            solve_func(funcs,new_a,new_b,deg,interval_data,root_tracker,tols,max_level)
+            solve_func(method,funcs,new_a,new_b,deg,interval_data,root_tracker,tols,max_level)
             root_tracker.keep_possible_duplicates()
     print("\rPercent Finished: 100%{}".format(' '*50))
 
@@ -166,7 +166,7 @@ def solve(funcs, a, b, rel_approx_tol=1.e-8, abs_approx_tol=1.e-12,
     if return_potentials:
         return root_tracker.roots, root_tracker.potential_roots
     else:
-        return root_tracker.roots
+        return root_tracker.roots, interval_data.cond, interval_data.backcond, root_tracker.conds
 
 def transform(x,a,b):
     """Transforms points from the interval [-1,1] to the interval [a,b].
@@ -526,7 +526,7 @@ def solve_linear(coeffs):
                 A[row,col] = coeff[var_list[col]]
     #solve the system
     try:
-        return np.linalg.solve(A,-B)
+        return np.linalg.solve(A,-B), [np.linalg.cond(A)]
     except np.linalg.LinAlgError as e:
         if str(e) == 'Singular matrix':
             #if the system is dependent, then there are infinitely many roots
@@ -541,7 +541,7 @@ def solve_linear(coeffs):
             if not (U.shape[1]-1 in pivot_columns):
                 #independent
                 warnings.warn('System potentially has infinitely many roots')
-            return np.zeros([0,dim])
+            return np.zeros([0,dim]), np.zeros([0,dim])
 
 def getAbsApproxTol(func, deg, a, b):
     """ Gets an absolute approximation tolerance based on the assumption that
@@ -580,7 +580,7 @@ def getAbsApproxTol(func, deg, a, b):
     numSpots = (deg*2)**len(a) - (deg)**len(a)
     return np.max(tols)*10 / numSpots
 
-def subdivision_solve_nd(funcs,a,b,deg,interval_data,root_tracker,tols,max_level,good_degs=None,level=0):
+def subdivision_solve_nd(method,funcs,a,b,deg,interval_data,root_tracker,tols,max_level,good_degs=None,level=0):
     """Finds the common zeros of the given functions.
 
     All the zeros will be stored in root_tracker.
@@ -638,7 +638,7 @@ def subdivision_solve_nd(funcs,a,b,deg,interval_data,root_tracker,tols,max_level
         if coeff is None:
             intervals = get_subintervals(a,b,change_sign,None,None,None,approx_errors)
             for new_a, new_b in intervals:
-                subdivision_solve_nd(funcs,new_a,new_b,deg,interval_data,root_tracker,tols,max_level,level=level+1)
+                subdivision_solve_nd(method,funcs,new_a,new_b,deg,interval_data,root_tracker,tols,max_level,level=level+1)
             return
         else:
             #if the function changes sign on at least one subinterval, skip the checks
@@ -661,42 +661,42 @@ def subdivision_solve_nd(funcs,a,b,deg,interval_data,root_tracker,tols,max_level
     #Check if everything is linear
     if np.all(np.array([coeff.shape[0] for coeff in coeffs]) == 2):
         if deg != 2:
-            subdivision_solve_nd(funcs,a,b,2,interval_data,root_tracker,tols,max_level,good_degs,level)
+            subdivision_solve_nd(method,funcs,a,b,2,interval_data,root_tracker,tols,max_level,good_degs,level)
             return
-        zero = solve_linear(coeffs)
+        zero,cond = solve_linear(coeffs)
         #Store the information and exit
         zero = transform(good_zeros_nd(zero.reshape([1,dim]),good_zeros_tol,good_zeros_tol),a,b)
         interval_data.track_interval("Base Case", [a,b])
-        root_tracker.add_roots(zero, a, b, "Base Case")
+        root_tracker.add_roots(zero, cond, a, b, "Base Case")
 
     #Check if anything is linear
 #     elif np.any(np.array([coeff.shape[0] for coeff in coeffs]) == 2):
 #         #Subdivide but run some checks on the intervals first
 #         intervals = get_subintervals(a,b,np.arange(dim),interval_data,cheb_approx_list,change_sign,approx_errors,True)
 #         for new_a, new_b in intervals:
-#             subdivision_solve_nd(funcs,new_a,new_b,deg,interval_data,root_tracker,tols,max_level,good_degs,level+1)
+#             subdivision_solve_nd(method,funcs,new_a,new_b,deg,interval_data,root_tracker,tols,max_level,good_degs,level+1)
 
     #Runs the same things as above, but we want to get rid of that eventually so keep them seperate.
     elif np.any(np.array([coeff.shape[0] for coeff in coeffs]) > 5) or not good_approx:
         intervals = get_subintervals(a,b,np.arange(dim),interval_data,cheb_approx_list,change_sign,approx_errors,True)
         for new_a, new_b in intervals:
-            subdivision_solve_nd(funcs,new_a,new_b,deg,interval_data,root_tracker,tols,max_level,good_degs,level+1)
+            subdivision_solve_nd(method,funcs,new_a,new_b,deg,interval_data,root_tracker,tols,max_level,good_degs,level+1)
 
     #Solve using spectral methods if stable.
     else:
         polys = [MultiCheb(coeff, lead_term = [coeff.shape[0]-1], clean_zeros = False) for coeff in coeffs]
         try:
-            zeros,cond,cond_back = multiplication(polys, max_cond_num=tols.max_cond_num)
-            interval_data.cond = max(cond,interval_data.cond)
-            interval_data.cond_back =  max(cond_back,interval_data.cond_back)
+            zeros,cond,backcond,cond_eig = multiplication(polys, max_cond_num=tols.max_cond_num, method=method)
+            interval_data.cond     =  max(cond,    interval_data.cond)
+            interval_data.backcond =  max(backcond,interval_data.backcond)
             zeros = transform(good_zeros_nd(zeros,good_zeros_tol,good_zeros_tol),a,b)
             interval_data.track_interval("Spectral", [a,b])
-            root_tracker.add_roots(zeros, a, b, "Spectral")
+            root_tracker.add_roots(zeros,cond_eig, a, b, "Spectral")
         except ConditioningError as e:
             #Subdivide but run some checks on the intervals first
             intervals = get_subintervals(a,b,np.arange(dim),interval_data,cheb_approx_list,change_sign,approx_errors,True)
             for new_a, new_b in intervals:
-                subdivision_solve_nd(funcs,new_a,new_b,deg,interval_data,root_tracker,tols,max_level,good_degs,level+1)
+                subdivision_solve_nd(method,funcs,new_a,new_b,deg,interval_data,root_tracker,tols,max_level,good_degs,level+1)
 
 def trim_coeffs(coeffs, abs_approx_tol, rel_approx_tol, trim_zero_tol, inf_norms, errors):
     """Trim the coefficient matrices so they are stable and choose a direction to divide in.
