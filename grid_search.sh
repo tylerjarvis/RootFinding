d35d8e48147443f275c3e9d8adf54a7a58cc1263
Lukas Erekson <Lukas.Erekson@gmail.com>
2020-02-19 23:33:08 -0700

    Updated script to also zip up the dictionaries at the end.

--- grid_search.sh
+++ grid_search.sh
@@ -11,3 +11,8 @@ python3 tol_gs.py svd 3 $> tol_gs_svd_3.txt &
 python3 tol_gs.py tvb 1 $> tol_gs_tvb_1.txt &
 python3 tol_gs.py tvb 2 $> tol_gs_tvb_2.txt &
 python3 tol_gs.py tvb 3 $> tol_gs_tvb_3.txt &
+
+# Consolidate the dictionaries of each type at the end
+python3 tests/chebsuite_tests/zip_dicts.py tests/chebsuite_tests/tols2_tvb tvb_2.pkl
+python3 tests/chebsuite_tests/zip_dicts.py tests/chebsuite_tests/tols2_svd svd_2.pkl
+python3 tests/chebsuite_tests/zip_dicts.py tests/chebsuite_tests/tols2_qrt qrt_2.pkl
