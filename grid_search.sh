b3b44a9a204b0d70614044976bb1c13bbe8e55f2
Lukas Erekson <Lukas.Erekson@gmail.com>
2020-03-24 19:06:11 -0600

    Updated grid_search.sh script to output error.

--- grid_search.sh
+++ grid_search.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 tol_gs.py svd 9 $> svd_deg_9.txt & 2> svd_deg_9.err &
-python3 tol_gs.py svd 15 $> svd_deg_15.txt & 2> svd_deg_15.err &
-python3 tol_gs.py svd 20 $> svd_deg_20.txt & 2> svd_deg_20.err &
-python3 tol_gs.py svd 25 $> svd_deg_25.txt & 2> svd_deg_25.err &
+python3 tol_gs.py svd 9 $> svd_deg_9.txt 2> svd_deg_9.err &
+python3 tol_gs.py svd 15 $> svd_deg_15.txt 2> svd_deg_15.err &
+python3 tol_gs.py svd 20 $> svd_deg_20.txt 2> svd_deg_20.err &
+python3 tol_gs.py svd 25 $> svd_deg_25.txt 2> svd_deg_25.err &
