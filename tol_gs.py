86cfa8f5e1e900922318d90a6a6b00c84098f31d
Lukas Erekson <Lukas.Erekson@gmail.com>
2020-03-26 17:05:34 -0600

    Added rel_approx_tols to test in our grid search.

--- tol_gs.py
+++ tol_gs.py
@@ -505,7 +505,7 @@ if __name__ == "__main__":
     test_num_dict = {n+1:test_nums[n] for n in range(num_tests)}
 
     # Define all the tolerances to try
-    rel_approx_tol = [1e-8]
+    rel_approx_tol = [1e-8, 1e-12, 1e-15]
     abs_approx_tol = [1e-12]
     max_cond_num = [10.**i for i in [7,9]]
     good_zeros_tol = [1e-5] 
