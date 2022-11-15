2c431710a836f2f2dea01b58463fa21009dd6308
Sam Goldrup <sgoldrup3@gmail.com>
2022-11-15 11:54:55 -0700

    returning Bounding Boxes

--- yroots/test_Combined_Solver.py
+++ yroots/test_Combined_Solver.py
@@ -7,98 +7,11 @@ import ChebyshevSubdivisionSolver
 import pytest
 from polynomial import MultiCheb
 from utils import transform
+from Combined_Solver import solver
 
 f = lambda x,y: (x-1)*(np.cos(x*y**2)+2)
 g = lambda x,y: np.sin(8*np.pi*y)*(np.cos(x*y)+2)
 f_deg,g_deg = 20,20
-
-def solver(funcs,a,b,guess_degs,rescale=False,rel_approx_tol=1.e-15, abs_approx_tol=1.e-12,exact=False):
-    """
-    Finds the roots of the system of functions
-
-    parameters
-    ----------
-    funcs: list
-    list of the vectorized functions (R^n --> R)
-    a: ndarray
-    lower bound on the search interval
-    b: ndarray
-    upper bound on the search interval
-    guess_degs: list
-    guess of the best approximation degree for each function
-    rescale: bool
-    whether to rescale the approximation by inf_norm or not
-    rel_approx_tol: float
-    relative approximation tolerance
-    abs_approx_tol: float
-    absolute approximation tolerance
-
-    returns
-    -------
-    ndarray:
-    the yroots of the system of functions
-    """
-    #TODO: allow for a,b to deafult to ones and negative ones
-    #TODO: handle case for when input degree is less than the approximation degree that was used
-    #TODO: decide whether to have the guess_deg input default, and what it would be (could the approximation degree used work), maybe they need to know their degree
-    #TODO: handle for case that input degree is above max_deg (provide a warning)
-    #TODO: maybe next we can include an option to return the bounding boxes
-
-    if len(a) != len(b):
-        raise ValueError("Dimension mismatch in intervals.")
-    
-    if (b<=a).any():
-        raise ValueError("At least one lower bound is >= an upper bound.")
-    
-    is_neg1_1 = True
-    arr_neg1 = np.array([-1]*len(a)) #what if a>b
-    arr_1 = np.ones(len(a))
-
-    if np.allclose(arr_neg1,a,rtol=1e-08) and np.allclose(arr_1,b,rtol=1e-08):
-        pass
-    else:
-        is_neg1_1 = False
-    
-    is_multi_cheb_arr = []
-
-    for func in funcs: #USE
-        if isinstance(func,MultiCheb):
-            is_multi_cheb_arr.append(True)
-            pass
-        else:
-            is_multi_cheb_arr.append(False)
-
-    is_multi_cheb_arr = np.array(is_multi_cheb_arr)
-    funcs = np.array(funcs)
-
-    MultiCheb_idxs = list(np.where(is_multi_cheb_arr==1)[0])
-    non_MultiCheb_idxs = list(np.where(is_multi_cheb_arr==0)[0])
-
-    errs = np.array([0]*len(funcs))
-
-    for idx in non_MultiCheb_idxs:
-        approx = M_maker.M_maker(funcs[idx],arr_neg1,arr_1,guess_degs[idx],rel_approx_tol,abs_approx_tol)
-        if rescale:
-            funcs[idx] = MultiCheb(approx.M_rescaled)
-        else:
-            funcs[idx] = MultiCheb(approx.M)
-        errs[idx] = approx.err
-
-    for idx in MultiCheb_idxs:
-        approx = M_maker.M_maker(funcs[idx],arr_neg1,arr_1,guess_degs[idx],rel_approx_tol,abs_approx_tol)
-        if rescale:
-            funcs[idx] = MultiCheb(approx.M_rescaled)
-        else:
-            funcs[idx] = MultiCheb(approx.M)
-
-    funcs = [func.coeff for func in funcs]
-    yroots = np.array(ChebyshevSubdivisionSolver.solveChebyshevSubdivision(funcs,errs,exact))
-
-    #transform doesn't work on empty arrays
-    if is_neg1_1 == False and len(yroots) > 0:
-        yroots = transform(yroots,a,b)
-
-    return yroots
 
 def solver_check(funcs,a,b):
     """
@@ -185,6 +98,8 @@ def test_exact_option():
 
     assert norm_yroots_exact <= norm_yroots_non_exact
 
+
+    #def testreturnBoundingBoxes():
 
 #WHAT CAN WE TEST ABOUT THIS CODE
 #WE CAN CHECK THAT IT PRESERVES WHAT ERIKs solver does when it is given the approximations
