83c8784c1fe2d16d5547453ade1fc1c1b3d58b09
Sam Goldrup <sgoldrup3@gmail.com>
2022-12-29 13:07:09 -1000

    add documentation strings to tests

--- yroots/test_Combined_Solver.py
+++ yroots/test_Combined_Solver.py
@@ -1,5 +1,7 @@
 """
-A solid 2 dimensional check before I hit the pull request.
+I run all these checks in two dimensions before hitting a pull request.
+TODO: add checks in higher dimensions or make sure that these checks are
+representative of higher dimensions
 """
 import numpy as np
 import yroots.M_maker as M_maker
@@ -17,69 +19,70 @@ f_deg,g_deg = 20,20
 
 def solver_check(funcs,a,b):
     """
-    raw functions on [-1,1]^n
+    parameters
+    ----------
+    funcs: list 
+    collection of callable functions on [-1,1]^n
+    a,b: ndarray
+    lower and upper bounds
+
+    returns
+    -------
+    bool: whether or not the solver accomplishes the same result as plugging the output of M_maker to solveChebyshevSubdivision
     """
 
     f,g = funcs
     yroots_1 = solve(funcs,a,b)
-    print("great")
 
-    arr_neg1 = np.array([-1]*len(a)) #what if a>b
+    arr_neg1 = np.array([-1]*len(a))
     arr_1 = np.ones(len(a))
 
-    #hate to do this
     f_approx = M_maker.M_maker(f,arr_neg1,arr_1,f_deg)
     g_approx = M_maker.M_maker(g,arr_neg1,arr_1,g_deg)
     
-    #bug with multicheb objects
-    print("trying this guy")
+    #TODO: make sure plugging in multicheb objects for [f_approx.M,g_approx.M]
     yroots_2 = np.array(ChebyshevSubdivisionSolver.solveChebyshevSubdivision([f_approx.M,g_approx.M],np.array([f_approx.err,g_approx.err])))
-    if len(yroots_2) > 0: #transform doesn't work on empty arrays
+    if len(yroots_2) > 0: #because transform doesn't work on empty arrays
         yroots_2 = transform(yroots_2,a,b)
+    else: #case where no roots are found
+        return len(yroots_1) == 0 
 
     return np.allclose(yroots_1,yroots_2)
 
-#TODO: can I delete all the stuff below? find out where it is taken care of
-#TODO: need docstrings for tests
-
-#CASES
-    #not neg1_1 and not all multicheb
-    #neg1_1 and not all multicheb
-    #not neg1_1 and all multicheb
-    #both neg1_1 and all multicheb
-
 def test_solver():
+    """
+    runs solver_check() on the six following cases:
+    (a) non-[-1,1]^n region of the space
+        (i) non-MultiCheb objects
+        (ii) some MultiCheb, some non-MultiCheb objects
+        (iii) MultiCheb objects
+    (b) ^same as above, but on [-1,1]^n region of the space
+    """
     a = -1*np.random.random(2)
     b = np.random.random(2)
-    arr_neg1 = np.array([-1]*len(a)) #what if a>b
+    arr_neg1 = np.array([-1]*len(a))
     arr_1 = np.ones(len(a))
 
-    #TODO: multicheb object on the correct domain
-    print("now approximating the first one")
     g_approx = M_maker.M_maker(g,arr_neg1,arr_1,g_deg)
-    print("now approximating the second one")
     h = MultiCheb(g_approx.M)
     f_approx = M_maker.M_maker(f,arr_neg1,arr_1,f_deg)
     k = MultiCheb(f_approx.M)
 
-    print("first 3 tests")
-    assert solver_check([f,g],a,b) == True #none multicheb and neg1_1
-    print("test 1")
-    assert solver_check([f,h],a,b) == True #some multicheb and neg1_1
-    print("test 2")
-    assert solver_check([h,k],a,b) == True #all multicheb and neg1_1
-    print("test 3")
+    assert solver_check([f,g],a,b) == True #none multicheb and not neg1_1
+    assert solver_check([f,h],a,b) == True #some multicheb and not neg1_1
+    assert solver_check([h,k],a,b) == True #all multicheb and not neg1_1
     b = np.ones(2).astype(float)
     a = -1*b
-    print("second 3 tests")
-    assert solver_check([f,g],a,b) == True #none multicheb and not neg1_1
-    print("test 1")
-    assert solver_check([k,g],a,b) == True #some multicheb and not neg1_1
-    print("test 2")
-    assert solver_check([h,k],a,b) == True #all multicheb and not neg1_1
-    print("test 3")
+    assert solver_check([f,g],a,b) == True #none multicheb and neg1_1
+    assert solver_check([k,g],a,b) == True #some multicheb and neg1_1
+    assert solver_check([h,k],a,b) == True #all multicheb and neg1_1
 
 def test_bad_intervals():
+    """
+    tests to make sure bad intervals get rejected by solve:
+    (a) a lower bound is greater than an upper bound
+    (b) the bounding arrays are unequal in length
+    """
     a,b = np.array([1,-1]),np.array([1,1])
     funcs = [f,g]
     with pytest.raises(ValueError) as excinfo:
@@ -92,6 +95,12 @@ def test_bad_intervals():
     assert excinfo.value.args[0] == "Dimension mismatch in intervals."
 
 def test_exact_option():
+    """
+    Solve has an "exact" option. 
+    This tests that option on test case 2.3 from chebfun2_suite.
+    We find the roots using the exact method and non-exact method.
+    Then we make sure we got the same roots between the two, and that those roots are correct.
+    """
     f = lambda x,y: np.sin(4*(x + y/10 + np.pi/10))
     g = lambda x,y: np.cos(2*(x-2*y+ np.pi/7))
     a,b = np.array([-1,-1]),np.array([1,1])
@@ -120,6 +129,11 @@ def test_exact_option():
     assert np.allclose(yroots_non_exact,chebfun_roots)
 
 def testreturnBoundingBoxes():
+    """
+    Solve has an option to return the bounding boxes on the roots. 
+    This test makes sure each root lies within their respective box.
+    This uses test case "" from chebfun2_suite
+    """
     f = lambda x,y: np.sin(4*(x + y/10 + np.pi/10))
     g = lambda x,y: np.cos(2*(x-2*y+ np.pi/7))
     a,b = np.array([-1,-1]),np.array([1,1])
@@ -135,6 +149,11 @@ def testreturnBoundingBoxes():
         assert box.__contains__(root) == True
 
 def testoutside_neg1_pos1():
+    """
+    Let the search interval be larger than [-1,1]^n.
+    Assert that each root is in its respective box.
+    This uses test case "" from chebfun2_suite
+    """
     f = lambda x,y: 2*x*y*np.cos(y**2)*np.cos(2*x)-np.cos(x*y)
     g = lambda x,y: 2*np.sin(x*y**2)*np.sin(3*x*y)-np.sin(x*y)
     a,b = np.array([-2,-2]), np.array([2,2])
@@ -148,6 +167,10 @@ def testoutside_neg1_pos1():
         assert box.__contains__(root) == True
 
 def test_default_nodeg():
+    """
+    Checks that the solver gets the correct solver when no guess degree is specified.
+    Using test case "" from chebfun2_suite.
+    """
     f = lambda x,y: np.sin(4*(x + y/10 + np.pi/10))
     g = lambda x,y: np.cos(2*(x-2*y+ np.pi/7))
     a,b = np.array([-1,-1]),np.array([1,1])
@@ -167,6 +190,12 @@ def test_default_nodeg():
     assert np.allclose(yroots,chebfun_roots)
 
 def test_deg_inf():
+    """
+    Tests the logic in Combined_Solver.py that detects which functions are MultiCheb, non-MultiCheb
+    and which functions can be treated like polynomials. This information is used to make smart degree
+    guesses, and the logic used to make the guesses is tested as well.
+    TODO: make this not be a direct copy and paste from Combined_Solver.py
+    """
     f = lambda x,y: y**2-x**3
     g = lambda x,y: (y+.1)**3-(x-.1)**2
     h = lambda x,y: np.cos(2*(x-2*y+ np.pi/7))
