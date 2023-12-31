bba8771a038d40bff6c125780af20c6aa044d7b0
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2023-12-31 12:52:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/tylerjarvis/RootFinding',
+    url='https://github.com/janeslagle/RootFinding',
     version='0.1.0',
     zip_safe=False,
 )
